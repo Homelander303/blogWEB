333c5bad97d3db9410658532f17273362ca90643
Homelander303 <109917231+Homelander303@users.noreply.github.com>
2022-11-04 13:14:02 +0600

    Add files via upload

---
+++ admin/inc/footer.php
@@ -0,0 +1,12 @@
+<div class="clear">
+        </div>
+    </div>
+    <div class="clear">
+    </div>
+    <div id="site_info">
+        <p>
+         &copy; Copyright <a href="http://www.facebook.com/mahfuz.rahman.90226628/">Mahfuzur rahman</a>. All Rights Reserved.
+        </p>
+    </div>
+</body>
+</html>
